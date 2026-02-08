0e751c9e439a3a3aef0389ee975c3826708811db
Grajales-Camilo <juangrajales305127@correo.itm.edu.co>
2026-02-08 15:42:50 -0500

    Build en Vite para publicar en GitHub

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import Hero from './components/Hero';
 import TheLegacy from './components/TheLegacy';
 import BinaryMission from './components/BinaryMission';
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Fundacion_AG/', // Esta línea es la clave para publicar en GitHub Pages
   server: {
     port: 3000
   }
